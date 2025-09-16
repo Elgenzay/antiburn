be0f627739f15c574f46b870a2e276041299c933
Jason <jason@elg.gg>
2025-09-16 14:15:55 -0400

    Fixes

--- watchface/index.js
+++ watchface/index.js
@@ -4,7 +4,6 @@ import { Time } from "@zos/sensor";
 const COLOR = 0x888888;
 const W = 480;
 const SHIFT_PX = 16;
-const DEBUG = false;
 
 function pad2(n) {
     return n < 10 ? "0" + n : "" + n;
@@ -31,7 +30,6 @@ WatchFace({
         const timeText = createWidgetFromBase(base_time, 100);
         const weekdayText = createWidgetFromBase(base_weekday, 32);
         const dateText = createWidgetFromBase(base_date, 32);
-
         const ampmText = createWidget(widget.TEXT, {
             ...base_ampm,
             text_size: 32, color: COLOR,
@@ -45,34 +43,28 @@ WatchFace({
 
         const wdNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-        const tick = () => {
-            let h = time.getFormatHour?.() ?? time.getHours();
+        const computeDy = () => {
+            const h24 = time.getHours();
             const m = time.getMinutes();
             const s = time.getSeconds?.() ?? 0;
-            let am = time.am;
-
-            if (h > 12) {
-                h -= 12;
-                am = false;
-            }
-
-            const cycle = DEBUG ? 5 : 3600;
-            const totalSec = time.getHours() * 3600 + m * 60 + s;
+            const cycle = 3600;
+            const totalSec = h24 * 3600 + m * 60 + s;
             const t = (totalSec % cycle) / cycle;
             const tri = t < 0.5 ? (t * 2) : (2 - t * 2);
-            const dy = Math.round((tri * 2 - 1) * SHIFT_PX);
+            return Math.round((tri * 2 - 1) * SHIFT_PX);
+        };
 
-            if (dy !== lastDy) {
-                timeText.setProperty(prop.MORE, { x: base_time.x, y: base_time.y + dy, w: base_time.w, h: base_time.h });
-                weekdayText.setProperty(prop.MORE, { x: base_weekday.x, y: base_weekday.y + dy, w: base_weekday.w, h: base_weekday.h });
-                dateText.setProperty(prop.MORE, { x: base_date.x, y: base_date.y + dy, w: base_date.w, h: base_date.h });
-                ampmText.setProperty(prop.MORE, { x: base_ampm.x, y: base_ampm.y + dy, w: base_ampm.w, h: base_ampm.h });
-                lastDy = dy;
-            }
+        const paint = () => {
+            const h24 = time.getHours();
+            const mins = time.getMinutes();
 
-            if (m !== lastMin) {
-                lastMin = m;
-                timeText.setProperty(prop.TEXT, `${pad2(h)}:${pad2(m)}`);
+            let am = h24 < 12;
+            let h12 = h24 % 12;
+            if (h12 === 0) h12 = 12;
+
+            if (mins !== lastMin) {
+                lastMin = mins;
+                timeText.setProperty(prop.TEXT, `${pad2(h12)}:${pad2(mins)}`);
                 ampmText.setProperty(prop.TEXT, am ? "AM" : "PM");
             }
 
@@ -85,14 +77,24 @@ WatchFace({
             const d = time.getDate();
             const mo = time.getMonth();
             const y = time.getFullYear();
-            dateText.setProperty(prop.TEXT, `${pad2(mo)}/${pad2(d)}/${pad2(y)}`);
+            dateText.setProperty(prop.TEXT, `${pad2(mo)}/${pad2(d)}/${y}`);
+            const dy = computeDy();
+
+            if (dy !== lastDy) {
+                timeText.setProperty(prop.MORE, { x: base_time.x, y: base_time.y + dy, w: base_time.w, h: base_time.h });
+                weekdayText.setProperty(prop.MORE, { x: base_weekday.x, y: base_weekday.y + dy, w: base_weekday.w, h: base_weekday.h });
+                dateText.setProperty(prop.MORE, { x: base_date.x, y: base_date.y + dy, w: base_date.w, h: base_date.h });
+                ampmText.setProperty(prop.MORE, { x: base_ampm.x, y: base_ampm.y + dy, w: base_ampm.w, h: base_ampm.h });
+                lastDy = dy;
+            }
         };
 
-        this._timer = setInterval(tick, DEBUG ? 1000 : 60000);
-        tick();
+        time.onPerMinute?.(paint);
+        paint();
+        this._paint = paint;
     },
 
-    onDestroy() {
-        if (this._timer) clearInterval(this._timer);
+    onResume() {
+        this._paint && this._paint();
     }
 });
